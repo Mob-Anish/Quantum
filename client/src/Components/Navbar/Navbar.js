fe145f2d9ebce174241db9a383bc0ee3d2eca033
Mob-Anish <mobanish16@gmail.com>
2022-08-15 14:43:40 +0545

    navbar updated

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,4 +1,9 @@
 import "./Navbar.css";
+import * as routes from "../../Constants/routes";
+import { GrTwitter } from "react-icons/gr";
+import { BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
+
+const year = new Date().getFullYear();
 
 const Navbar = () => {
   return (
@@ -25,6 +30,35 @@ const Navbar = () => {
               </div>
             </li>
           </ul>
+        </div>
+        <div className="quantum__navbar__footer">
+          <div
+            className="quantum__social__links"
+            style={{ marginBottom: "3rem" }}
+          >
+            <a href={routes.TWITTER} target="_blank" rel="noopener noreferrer">
+              <GrTwitter />
+            </a>
+            <a
+              href={routes.INSTAGRAM}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsInstagram />
+            </a>
+            <a href={routes.GITHUB} target="_blank" rel="noopener noreferrer">
+              <BsGithub />
+            </a>
+            <a href={routes.LINKEDIN} target="_blank" rel="noopener noreferrer">
+              <BsLinkedin />
+            </a>
+          </div>
+          <div
+            className="quantum__navbar__copyright"
+            style={{ fontSize: "1.4rem" }}
+          >
+            &copy; {year} Quantum Anish Manandhar
+          </div>
         </div>
       </div>
     </div>
--- client/src/Constants/routes.js
+++ client/src/Constants/routes.js
@@ -2,3 +2,10 @@ export const HOME = "/";
 export const ONSTART = "/readytogo/next";
 export const REGISTER = "/readytogo/create-account/:token";
 export const ONBOARD = "/readytogo/onboard/:token";
+
+// External Link
+export const TWITTER = "https://mobile.twitter.com/mob_anish";
+export const INSTAGRAM = "https://www.instagram.com/shoya__711/";
+export const GITHUB = "https://github.com/Mob-Anish";
+export const LINKEDIN =
+  "https://www.linkedin.com/in/anish-manandhar-51a1b8243/";
