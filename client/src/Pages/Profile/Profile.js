6702b606339ae8f241d634ec57187de03dcaa5e6
Mob-Anish <mobanish16@gmail.com>
2023-01-05 20:16:01 +0545

    restructuring the profile page

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -7,7 +7,10 @@ const Profile = () => {
   return (
     <div className="quantum__profile--container">
       <div className="profile__container--sidebar">
-        <div className="logo" style={{ marginBottom: "5rem", padding: "1rem" }}>
+        <div
+          className="logo"
+          style={{ marginBottom: "5rem", padding: "1rem 1.5rem" }}
+        >
           <Link to={routes.HOME} style={{ textDecoration: "none" }}>
             <span style={{ color: "#ffffff", fontSize: "2.8rem" }}>
               Quantum
@@ -22,7 +25,7 @@ const Profile = () => {
               </div>
             </Link>
           </div>
-          <div className="name" style={{ marginTop: "3rem" }}>
+          <div className="name" style={{ margin: "2rem 0 6rem 0" }}>
             <h1 className="fullname">
               <Link to="/">Hinata Shoyo</Link>
             </h1>
@@ -34,17 +37,20 @@ const Profile = () => {
             </h2>
           </div>
           <div className="tagline">
+            <h2 style={{ marginBottom: "1rem" }}>Tagline :</h2>
             <p style={{ fontSize: "1.5rem", lineHeight: "1.375" }}>
               Senior Software Engineer. Google Developers Expert. Entrepreneur.
               Mountain Climber. Cat Lover. Gardener. World traveler.
             </p>
           </div>
           <div className="membership__date">
+            <h2 style={{ marginBottom: "1rem" }}>Joined :</h2>
             <span style={{ fontSize: "1.5rem" }}>
               📅 Member Since Jul, 2022
             </span>
           </div>
           <div className="email__address">
+            <h2 style={{ marginBottom: "1rem" }}>Email Address :</h2>
             <Link
               to="#"
               onClick={(e) => {
@@ -57,12 +63,168 @@ const Profile = () => {
           </div>
         </div>
       </div>
-      <div className="profile__container--main" style={{ padding: "4rem" }}>
-        <div className="title" style={{ marginBottom: "3rem" }}>
+      <div
+        className="profile__container--main"
+        style={{ padding: "7rem 5rem" }}
+      >
+        <div className="title" style={{ marginBottom: "4rem" }}>
           <h1>Stories 📃</h1>
         </div>
         <div className="posts">
-          <div className="user__feed--container" style={{ width: "110rem" }}>
+          <div className="user__feed--container" style={{ width: "105rem" }}>
+            <div className="user__feed--profile">
+              <div className="feed__username--date">
+                <Link to="/" className="user__feed--date">
+                  <span style={{ fontSize: "1.5rem" }}>Dec 26, 2022</span>
+                </Link>
+              </div>
+            </div>
+            <div
+              className="user__feed--content"
+              style={{ display: "flex", alignItems: "flex-start" }}
+            >
+              <div
+                className="feed__content--1"
+                style={{ paddingRight: "1.25rem" }}
+              >
+                <div className="user__feed--title">
+                  <h1 style={{ fontSize: "2.5rem" }}>
+                    <Link to="/" target="_blank">
+                      Hello World Welcome to the Programming!!
+                    </Link>
+                  </h1>
+                </div>
+                <div
+                  className="user__feed--description"
+                  style={{ display: "inline-block" }}
+                >
+                  <p
+                    style={{
+                      lineHeight: "1.375",
+                      wordBreak: "normal",
+                      overflowWrap: "anywhere",
+                    }}
+                  >
+                    <Link to="/" target="_blank">
+                      I am an Engineering Student. I wanted to take Computer
+                      Science but due to some reasons, I ended up taking
+                      Electronics and Communication. But I never lost interest
+                      in coding which is the reason I'm cur…
+                    </Link>
+                  </p>
+                </div>
+              </div>
+              <div className="feed__content--2">
+                <Link to="/" target="_blank" className="feed__content--image">
+                  <img src={ProfleImg} alt="title" className="feed__image" />
+                </Link>
+              </div>
+            </div>
+            {/* <div className="user__feed--reaction"></div> */}
+          </div>
+          <div className="user__feed--container" style={{ width: "105rem" }}>
+            <div className="user__feed--profile">
+              <div className="feed__username--date">
+                <Link to="/" className="user__feed--date">
+                  <span style={{ fontSize: "1.5rem" }}>Dec 26, 2022</span>
+                </Link>
+              </div>
+            </div>
+            <div
+              className="user__feed--content"
+              style={{ display: "flex", alignItems: "flex-start" }}
+            >
+              <div
+                className="feed__content--1"
+                style={{ paddingRight: "1.25rem" }}
+              >
+                <div className="user__feed--title">
+                  <h1 style={{ fontSize: "2.5rem" }}>
+                    <Link to="/" target="_blank">
+                      Hello World Welcome to the Programming!!
+                    </Link>
+                  </h1>
+                </div>
+                <div
+                  className="user__feed--description"
+                  style={{ display: "inline-block" }}
+                >
+                  <p
+                    style={{
+                      lineHeight: "1.375",
+                      wordBreak: "normal",
+                      overflowWrap: "anywhere",
+                    }}
+                  >
+                    <Link to="/" target="_blank">
+                      I am an Engineering Student. I wanted to take Computer
+                      Science but due to some reasons, I ended up taking
+                      Electronics and Communication. But I never lost interest
+                      in coding which is the reason I'm cur…
+                    </Link>
+                  </p>
+                </div>
+              </div>
+              <div className="feed__content--2">
+                <Link to="/" target="_blank" className="feed__content--image">
+                  <img src={ProfleImg} alt="title" className="feed__image" />
+                </Link>
+              </div>
+            </div>
+            {/* <div className="user__feed--reaction"></div> */}
+          </div>
+          <div className="user__feed--container" style={{ width: "105rem" }}>
+            <div className="user__feed--profile">
+              <div className="feed__username--date">
+                <Link to="/" className="user__feed--date">
+                  <span style={{ fontSize: "1.5rem" }}>Dec 26, 2022</span>
+                </Link>
+              </div>
+            </div>
+            <div
+              className="user__feed--content"
+              style={{ display: "flex", alignItems: "flex-start" }}
+            >
+              <div
+                className="feed__content--1"
+                style={{ paddingRight: "1.25rem" }}
+              >
+                <div className="user__feed--title">
+                  <h1 style={{ fontSize: "2.5rem" }}>
+                    <Link to="/" target="_blank">
+                      Hello World Welcome to the Programming!!
+                    </Link>
+                  </h1>
+                </div>
+                <div
+                  className="user__feed--description"
+                  style={{ display: "inline-block" }}
+                >
+                  <p
+                    style={{
+                      lineHeight: "1.375",
+                      wordBreak: "normal",
+                      overflowWrap: "anywhere",
+                    }}
+                  >
+                    <Link to="/" target="_blank">
+                      I am an Engineering Student. I wanted to take Computer
+                      Science but due to some reasons, I ended up taking
+                      Electronics and Communication. But I never lost interest
+                      in coding which is the reason I'm cur…
+                    </Link>
+                  </p>
+                </div>
+              </div>
+              <div className="feed__content--2">
+                <Link to="/" target="_blank" className="feed__content--image">
+                  <img src={ProfleImg} alt="title" className="feed__image" />
+                </Link>
+              </div>
+            </div>
+            {/* <div className="user__feed--reaction"></div> */}
+          </div>
+          <div className="user__feed--container" style={{ width: "105rem" }}>
             <div className="user__feed--profile">
               <div className="feed__username--date">
                 <Link to="/" className="user__feed--date">
